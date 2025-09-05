f3c0133c84c0078dc4240da3078421b594d62335
trob94 <trobins945754@gmail.com>
2025-09-05 06:55:30 -0500

    Create synthesize.py

---
+++ synthesize.py
@@ -0,0 +1,19 @@
+import boto3
+import sys
+
+# Read text file
+text = open('speech.txt').read()
+
+# Create audio
+polly = boto3.client('polly')
+audio = polly.synthesize_speech(Text=text, OutputFormat='mp3', VoiceId='Joanna')
+
+# Upload to S3
+s3 = boto3.client('s3')
+s3.put_object(
+    Bucket='your-bucket-name',
+    Key=f'polly-audio/{sys.argv[1]}',
+    Body=audio['AudioStream'].read()
+)
+
+print('Done!')
